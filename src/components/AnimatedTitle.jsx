e110efda5d3bd1d9544578af00960c513a724448
Wheeler Knight <wheelerknight19@gmail.com>
2025-02-18 16:41:44 -0600

    2-18-2025: Added animation to hero section - created skeleton for about component - added different styling - added scroll animation to the button - added animated title component - added animated title to hero section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,12 @@
+import { PropTypes } from 'prop-types'
+
+const AnimatedTitle = ({ title, containerClass }) => {
+  return <div></div>
+}
+
+AnimatedTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  containerClass: PropTypes.string,
+}
+
+export default AnimatedTitle
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { TiLocationArrow } from 'react-icons/ti'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/all'
+
 import Button from './Button'
+// import VideoPreview from './VideoPreview';
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1)
   const [hasClicked, setHasClicked] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [loadedVideos, setLoadedVideos] = useState(0)
 
   const totalVideos = 4
@@ -16,16 +23,75 @@ const Hero = () => {
 
   const handleMiniVdClick = () => {
     setHasClicked(true)
-    setCurrentIndex(upcomingVideoIndex)
+    setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1)
   }
 
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setLoading(false)
+    }
+  }, [loadedVideos])
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set('#next-video', { visibility: 'visible' })
+        gsap.to('#next-video', {
+          transformOrigin: 'center center',
+          scale: 1,
+          width: '100%',
+          height: '100%',
+          duration: 1,
+          ease: 'power1.inOut',
+          onStart: () => nextVdRef.current.play(),
+        })
+        gsap.from('#current-video', {
+          transformOrigin: 'center center',
+          scale: 0,
+          duration: 1.5,
+          ease: 'power1.inOut',
+        })
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  )
+
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 88% 90%, 0% 95%)',
+      borderRadius: '0% 0% 40% 10%',
+    })
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0% 0% 0% 0%',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true,
+      },
+    })
+  })
+
   const getVideoSrc = (index) => `../../public/videos/hero-${index}.mp4`
+
   const handleVideoLoad = () => {
     setLoadedVideos((prevLoadedVideos) => prevLoadedVideos + 1)
   }
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden ">
+      {loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
+
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -38,7 +104,7 @@ const Hero = () => {
             >
               <video
                 ref={nextVdRef}
-                src={getVideoSrc(upcomingVideoIndex)}
+                src={getVideoSrc((currentIndex % totalVideos) + 1)}
                 loop
                 muted
                 id="current-video"
@@ -46,10 +112,11 @@ const Hero = () => {
                 onLoadedData={handleVideoLoad}
               />
             </div>
+            {/* </VideoPreview >*/}
           </div>
 
           <video
-            ref={currentVdRef} // Use the new ref here
+            ref={nextVdRef}
             src={getVideoSrc(currentIndex)}
             loop
             muted
@@ -59,7 +126,7 @@ const Hero = () => {
           />
 
           <video
-            src={getVideoSrc(currentIndex === totalVideos ? 1 : currentIndex + 1)}
+            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
             autoPlay
             loop
             muted
@@ -67,29 +134,29 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
           />
         </div>
-        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75 ">
-          G<b>a</b>ming
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75 !text-5xl">
+          wheelerknight.com
         </h1>
 
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
-            <h1 className="special-font hero-heading">
-              redifi<b>n</b>ed
+            <h1 className="special-font hero-heading text-blue-75">
+              <b>C</b>Rea<b>t</b>ING
             </h1>
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-              Enter the Metagame Layer. <br /> Unleash the Play Economy.
+              projects I care about, <br /> with <b>detail</b> and <b>excitement</b>.
             </p>
             <Button
               id="watch-trailer"
-              title="Watch trailer"
+              title="Get In Touch"
               leftIcon={<TiLocationArrow />}
-              containerClass="!bg-yellow-300 flex-center gap-1"
+              containerClass="!bg-violet-300 flex-center gap-1"
             />
           </div>
         </div>
       </div>
-      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black ">
-        G<b>a</b>ming
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black !text-5xl">
+        wheelerknight.com
       </h1>
     </div>
   )
