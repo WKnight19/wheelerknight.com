c5407d0080916f856a35f1d16e50bdf6f700be5d
Wheeler Knight <wheelerknight19@gmail.com>
2025-02-18 14:11:25 -0600

    added all packages, configured eslint/prettier, added components, and added assets (Feb 18th, 2025)
    - added audio, video, images, fonts, icons, and animations
    - added tailwindcss and postcss (v, autoprefixer)
    - added animated hero and header
    *NOTE: source code for header and assets are not mine

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+export default {
+  semi: false, // Don't add semicolons at the end of statements
+  singleQuote: true, // Use single quotes instead of double quotes
+  trailingComma: 'es5', // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  tabWidth: 2, // Use 2 spaces for indentation
+  useTabs: false, // Don't use tabs for indentation
+  bracketSpacing: true, // Add spaces between brackets in objects and arrays
+  arrowParens: 'always', // Require parentheses around arrow function arguments (even single arguments)
+  printWidth: 100, // Maximum line length before wrapping
+}
